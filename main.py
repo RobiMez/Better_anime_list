fd375bd78dde75abd1e736c230d7939fd6543305
Robi <53259730+RobiMez@users.noreply.github.com>
2021-10-30 00:49:59 +0300

    added the add feature

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ logging.basicConfig(
     level=logging.INFO)
 
 TOKEN = dotenv.get_key('.env','TOKEN') or getenv('TOKEN')
-# COLLECTIONID = dotenv.get_key('.env','COLLECTIONID') or getenv('COLLECTIONID')
+COLLECTIONID = dotenv.get_key('.env','COLLECTIONID') or getenv('COLLECTIONID')
 ENDPOINT = dotenv.get_key('.env','ENDPOINT') or getenv('ENDPOINT')
 PROJECTID = dotenv.get_key('.env','PROJECTID') or getenv('PROJECTID')
 APIKEY = dotenv.get_key('.env','APIKEY') or getenv('APIKEY')
@@ -61,6 +61,7 @@ def create_account(update,context):
         context.bot.send_message(
             chat_id=update.effective_chat.id, 
             text="Already registered")
+
 def anime_search(update,context):
     if context.args :
         query = context.args
@@ -107,26 +108,75 @@ def anime(update,context):
     # archive = jikan.season_archive()
 
 
-def list_accounts(update,context):
-    result = users.list()
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, 
-        text=result)
-    
-result = database.create_collection('[NAME]', [], [], [])
-
-
+def add(update,context):
+    user_id = update.effective_user.id
+    print(context.args)
+    if len(context.args) == 1 :
+        # check single arg 
+        try:
+            # check numeric 
+            mal_id = int(context.args[0])
+            exists = database.list_documents(
+                COLLECTIONID,
+                filters=[f'userID={user_id}'])
+            if exists['sum'] == 0:
+                # if no doc exists create it 
+                payload = {
+                    "userID":user_id,
+                    "MalList":[mal_id]
+                    }
+                result = database.create_document(COLLECTIONID, payload)
+                context.bot.send_message(
+                    chat_id=update.effective_chat.id, 
+                    text=f'Successfully added {mal_id} to list ')
+                context.bot.send_message(
+                    chat_id=update.effective_chat.id, 
+                    text=result)
+            else:
+                doc_id = exists['documents'][0]['$id']
+                print(exists['documents'][0]['MalList'])
+                if mal_id in exists['documents'][0]['MalList']:
+                    context.bot.send_message(
+                    chat_id=update.effective_chat.id, 
+                    text=f'{mal_id} is already on your list')
+                else:
+                    print(doc_id)
+                    # update existing doc
+                    malist = list(exists['documents'][0]['MalList'])
+                    malist.append(int(mal_id))
+                    payload = {
+                        "userID":user_id,
+                        "MalList":malist
+                        }
+                    updated = database.update_document(
+                        COLLECTIONID, 
+                        doc_id, 
+                        payload)
+                    context.bot.send_message(
+                        chat_id=update.effective_chat.id, 
+                        text=f'updated list with {mal_id}')
+                    context.bot.send_message(
+                        chat_id=update.effective_chat.id, 
+                        text=payload)
+        except TypeError as err:
+            logging.error(err)
+    else:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, 
+            text='usage : /add {id}')
+        
+# watching ,completed ,on-hold ,dropped ,plan to watch 
 
 def main():
     "Main Execution"
     start_handler = CommandHandler('start', start)
     create_account_handler = CommandHandler('create_account', create_account)
-    list_accounts_handler = CommandHandler('list_accounts', list_accounts)
+    add_handler = CommandHandler('add', add)
     anime_search_handler = CommandHandler('anime_search', anime_search)
     anime_handler = CommandHandler('anime', anime)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(create_account_handler)
-    dispatcher.add_handler(list_accounts_handler)
+    dispatcher.add_handler(add_handler)
     dispatcher.add_handler(anime_search_handler)
     dispatcher.add_handler(anime_handler)
     
